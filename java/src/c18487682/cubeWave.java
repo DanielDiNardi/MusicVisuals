1a23e06dea383ac591d7adf4a712497cda0b4476
Danie Di Nardi <c18487682@mytudublin.ie>
2020-04-09 15:27:16 +0100

    colour cycle

--- java/src/c18487682/cubeWave.java
+++ java/src/c18487682/cubeWave.java
@@ -22,99 +22,92 @@ public class cubeWave extends Visual
     {
         colorMode(HSB);
         noCursor();
-        background(0);
         
         setFrameSize(256);
 
         startMinim();
-        loadAudio("Glinhorn Forest.mp3");
+        loadAudio("Timecop1983 - On the Run.mp3");
 
-        ortho(-1000, 1900, -2000, 2000, -2000, 2000);
+        ortho(-1000, 1900, -1000, 1000, -2000, 2000);
         //ortho(left, right, bottom, top, near, far);
-
-        for(int i = 0; i < stars.length; i++)
-        {
-            stars[i] = new Stars();
-        }
     }
 
     float ya = QUARTER_PI;
     float xa = -QUARTER_PI;
-    int col = 14;
-    int row = 14;
+    int col = 15;
+    int row = 15;
     float pz = width / 2;
     float px = width / 2;
     float py = height / 2;
     float amp = 0;
-    float maxD = dist(0, 0, width / 28f, width / 28f);
-    Stars[] stars = new Stars[100];
+    float maxD = dist(0, 0, 400, -400);
+    int colour = 0;
 
     public void draw()
     {
-        star();
-        //carpet();
+        if(colour < 255)
+        {
+            colour += getSmoothedAmplitude() * 8;
+        }
+        else
+        {
+            colour = 0;
+        }
+
+        background(0);
+        // fill(map(getSmoothedAmplitude(), 0f, 1f, 0f, 255f), 200f, 150f);
+        fill(colour, 200f, 150f);
+        carpet();
     }
 
     void carpet()
     {
         float x, y, z;
-            x = width / 14f;
-            y = 400;
-            z = width / 14f;
+        x = 50;
+        y = getSmoothedAmplitude() * 1000;
+        z = 50;
             
-            float offset = 0;
+        float offset = 0;
 
-            calculateAverageAmplitude();
-            translate(width/2, height/2, 0);
-            rotateX(xa);
-            rotateY(ya);
-            fill(map(getSmoothedAmplitude(), 0, 1, 0, 255), 255, 255);
-            stroke(0);
-            
-            for(int i = 0; i < col; i++)
-            {
-                for(int j = 0; j < row; j++)
-                {
-                    pushMatrix();
-
-                    float d = dist(j, i, x, z);
-                    offset = map(d, 0, maxD, -1, 1);
-                    float a = (amp) + offset;
-                    float h = map(sin(a), -1, 1, 0, y);
-
-                    translate(px, 0, 0);
-                    box(x, h, x);
-
-                    px -= x;
-                    
-                    popMatrix();
-                }
-                // offset += 0.5;
-
-                px = 650;
-
-                pz = z;
-                translate(0, 0, pz);
-
-            }
-            amp += getSmoothedAmplitude() * 1.5;
-    }
-
-    void star()
-    {
-        for(int i = 0; i < stars.length; i++)
-        {
-            stars[i].update();
-            show();
-        }
-    }
-
-    void show()
-    {
-        fill(255);
-        noStroke();
+        // calculateFFT();
+        // calculateFrequencyBands();
+        calculateAverageAmplitude();
         translate(width/2, height/2, 0);
-        ellipse(c18487682.Stars.x, c18487682.Stars.y, 8, 8);
+        rotateX(xa);
+        rotateY(ya);
+        stroke(0);
+        lights();
+
+        // println(getSmoothedBands());
+
+        for(int i = 0; i < col; i++)
+        {
+            for(int j = 0; j < row; j++)
+            
+            {
+                pushMatrix();
+
+                float d = dist(j * x, i * z, 400, 400);
+                offset = map(d, 0, maxD, -2, 1);
+                float a = (amp) + offset;
+                float h = map(sin(a), 0, 1, 0, y);
+
+                // fill(map(getSmoothedAmplitude(), 0f, 1f, 0f, 255f), 200f, 150f); 
+                translate(px, 0, 0);
+                box(x, h, x);
+
+                px -= x;
+                    
+                popMatrix();
+            }
+
+            px = 700;
+
+            pz = z;
+            translate(0, 0, pz);
+
+        }
+        amp += getSmoothedAmplitude() * 0.25f;
     }
 }
 
